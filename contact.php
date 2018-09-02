45bec0e24917cdecca461b79e22366b3acb9bae0
Saksham Gupta <saksham.gupta2016@vitstudent.ac.in>
2018-09-02 23:02:24 +0530

    Contact Update

--- contact.php
+++ contact.php
@@ -1,5 +1,17 @@
 <?php
-if($_POST["message"]) {
-    mail("gupta.saksham98@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="gupta.saksham98@gmail.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["gupta.saksham98@gmail.com"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
+
 ?>
